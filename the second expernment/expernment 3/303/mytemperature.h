8e7924c8ce64d1c3744cced3cf179fd73c585e6c
liyijing0086 <liyijing1964@outlook.com>
2025-12-21 12:20:01 +0800

    Delete the second expernment/expernment 3 directory

--- the second expernment/expernment 3/303/mytemperature.h
+++
@@ -1,2 +0,0 @@
-double celsius_to_fah(double cel);
-double fahrenheit_to_cels(double fah);
--- the second expernment/expernment 3/304/mytriangle.h
+++
@@ -1,2 +0,0 @@
-bool is_valid(double side1, double side2, double side3);
-double area(double side1, double side2, double side3);
